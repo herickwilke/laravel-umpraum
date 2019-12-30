366d0f4a25712feac4880f2babecd373b8e8cc43
herick <herickcwilke@gmail.com>
2019-12-30 08:32:18 -0300

    Relacionando registros de duas tabelas

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@
 */
 
 use App\Cliente;
+use App\Endereco;
 
 Route::get('/clientes', function () {
     $clientes = Cliente::all();
@@ -19,6 +20,27 @@ Route::get('/clientes', function () {
         echo "<p>ID: " . $c->id . "</p>";
         echo "<p>Nome: " . $c->nome . "</p>";
         echo "<p>Telefone: " . $c->telefone . "</p>";
+        //$e = Endereco::where('cliente_id', $c->id)->first();
+        echo "<p>Rua: " . $c->endereco->rua . "</p>";
+        echo "<p>Número: " . $c->endereco->numero . "</p>";
+        echo "<p>Bairro: " . $c->endereco->bairro . "</p>";
+        echo "<p>Cidade: " . $c->endereco->cidade . "</p>";
+        echo "<p>UF: " . $c->endereco->uf . "</p>";
+        echo "<p>CEP: " . $c->endereco->cep . "</p>";
+        echo "<hr>";
+    }
+});
+
+Route::get('/enderecos', function () {
+    $ends = Endereco::all();
+    foreach($ends as $e) {
+        echo "<p>ID cliente: " . $e->cliente_id . "</p>";
+        echo "<p>Rua: " . $e->rua . "</p>";
+        echo "<p>Número: " . $e->numero . "</p>";
+        echo "<p>Bairro: " . $e->bairro . "</p>";
+        echo "<p>Cidade: " . $e->cidade . "</p>";
+        echo "<p>UF: " . $e->uf . "</p>";
+        echo "<p>CEP: " . $e->cep . "</p>";
         echo "<hr>";
     }
 });
