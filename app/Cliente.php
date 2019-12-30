4a1b80019c8a4db5f3960a5908590df7b050df78
herick <herickcwilke@gmail.com>
2019-12-30 08:45:26 -0300

    Detalhado o has one no App\Cliente

--- app/Cliente.php
+++ app/Cliente.php
@@ -9,4 +9,10 @@ class Cliente extends Model
     public function endereco() {
         return $this->hasOne('App\Endereco');
     }
+
+    //ACIMA FUNCIONA, POIS SEGUE O PADRÃO DE CONVENÇÃO DO LARAVEL, ONDE ELE ENCONTRA OS CAMPOS CLIENTE_ID E ID.
+    //SE OS CAMPOS TIVESSEM NOMENCLATURAS DIFERENTES, O CORRETO SERIA DA SEGUINTE FORMA:
+    // public function endereco() {
+    //     return $this->hasOne('App\Endereco', 'cliente_id', 'id');
+    // }
 }
